a60500ed20677c631611bb4dacff05342fe2fde4
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-03-16 17:18:47 +0200

    Initial Commit

---
+++ lab3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab3
+//
+//  Created by Alexandra Negru on 16/03/2020.
+//  Copyright © 2020 Alexandra Negru. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
