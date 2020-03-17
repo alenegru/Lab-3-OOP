2272b602733004dfddda0244d4fe2e3e0b0dadbf
lauragoron <lauragoron@yahoo.com>
2020-03-17 11:42:22 +0200

    created .h and .cpp files for Tree class

---
+++ Tree.cpp
@@ -0,0 +1,10 @@
+#include "Tree.h"
+#include "Node.h"
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+Tree::Tree()
+{
+	root = NULL;
+}
---
+++ Tree.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Node.h"
+#include <iostream>
+using namespace std;
+class Tree 
+{
+private:
+	Node *root;
+
+public:
+	Tree();
+
+
+};
